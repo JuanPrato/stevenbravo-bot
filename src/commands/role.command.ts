ba32e3bb1962eab7e448ebd599ec3517941055c5
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-01-19 00:46:40 -0300

    roles db function & role command

--- src/commands/role.command.ts
+++ src/commands/role.command.ts
@@ -3,6 +3,12 @@ import { IAnswerable, ICommand } from "../types/command.type";
 import { createSlashCommand } from "../utils/discord.util";
 import { db } from "..";
 import { users as usersTable } from "../db/schema";
+import {
+  addRoleToPlan,
+  deletePlanByRole,
+  editRolePlan,
+  getAllRoles,
+} from "../lib/db.lib";
 
 enum Types {
   ADD = "agregar",
@@ -31,7 +37,33 @@ const RoleCommand: ICommand<Args> = {
 
     switch (args.type) {
       case Types.ADD: {
+        if (!args.planId)
+          throw new Error("Al agregar el id del plan es obligatorio");
+        await addRoleToPlan(args.role.id, args.planId);
+        break;
       }
+      case Types.MODIFY: {
+        if (!args.planId)
+          throw new Error("Al editar el id del plan es obligatorio");
+        await editRolePlan(args.role.id, args.planId);
+        break;
+      }
+      case Types.REMOVE: {
+        await deletePlanByRole(args.role.id);
+        break;
+      }
+      default:
+        {
+          const roles = await getAllRoles();
+          await message.reply(
+            `ROLES: ${roles.reduce(
+              (acc, role) => acc + `\n<@${role.roleId}> : ${role.planId}`,
+              ""
+            )}`
+          );
+          return;
+        }
+        await message.reply(`OPERACIÓN :${args.type} realizada con éxito`);
     }
   },
   slashCommand: createSlashCommand(
--- src/lib/db.lib.ts
+++ src/lib/db.lib.ts
@@ -1,5 +1,6 @@
+import { eq } from "drizzle-orm";
 import { db } from "..";
-import { users } from "../db/schema";
+import { roles, users } from "../db/schema";
 
 export async function saveOrUpdateUserEmail(
   userId: string,
@@ -9,4 +10,28 @@ export async function saveOrUpdateUserEmail(
     mail,
     userId,
   });
+}
+
+export async function getAllRoles() {
+  return db.select().from(roles);
+}
+
+export async function addRoleToPlan(role: string, plan: string) {
+  await db.insert(roles).values({
+    planId: plan,
+    roleId: role,
+  });
+}
+
+export async function editRolePlan(role: string, plan: string) {
+  await db
+    .update(roles)
+    .set({
+      roleId: role,
+    })
+    .where(eq(roles.planId, plan));
+}
+
+export async function deletePlanByRole(role: string) {
+  await db.delete(roles).where(eq(roles.roleId, role));
 }
