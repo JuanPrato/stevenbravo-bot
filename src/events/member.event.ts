73591cb14f39d2dd01d119f4541ea4b3a2cac6fc
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-01-21 19:01:02 -0300

    use config to annoounce

--- src/events/member.event.ts
+++ src/events/member.event.ts
@@ -1,15 +1,24 @@
 import { client } from "../app";
 import { Events, TextChannel } from "discord.js";
 import { createEmbedWithText } from "../utils/discord.util";
+import { db } from "..";
+import { config as configTable } from "../db/schema";
+import { eq } from "drizzle-orm";
 
 function channelIsTextChannel(c: any): c is TextChannel {
   return true;
 }
 
 client.on(Events.GuildMemberAdd, async (member) => {
+  const [config, ..._] = await db
+    .select()
+    .from(configTable)
+    .where(eq(configTable.guildId, member.guild.id));
   console.log("New member");
+  if (!config || !config.announceChannelId) return;
+
   const announceChannel: TextChannel = (await client.channels.fetch(
-    process.env.ANNOUNCE_CHANNEL_ID!
+    config.announceChannelId
   )) as TextChannel;
   await announceChannel.send({
     content: `${member}`,
